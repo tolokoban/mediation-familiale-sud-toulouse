aeaa635c2711ae7f14fcd357a2d70d82e6892cf7
Tolokoban <contact@tolokoban.org>
2020-09-20 19:46:04 +0200

    Updates

---
+++ precache-manifest.d249306f667d9c836e4bbb8598565b6c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0a7108f37196737ab4e62bdff76a055",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a24def5bbf0f8181c8f0",
+    "url": "./static/css/main.9a43922f.chunk.css"
+  },
+  {
+    "revision": "a24def5bbf0f8181c8f0",
+    "url": "./static/js/main.94987724.chunk.js"
+  },
+  {
+    "revision": "e1cbdc27d916ecd7b036",
+    "url": "./static/js/runtime-main.aa6f8073.js"
+  },
+  {
+    "revision": "0bf35770dd022c5070a8a677093416df",
+    "url": "./static/media/logo.0bf35770.svg"
+  },
+  {
+    "revision": "85d655066eb182e531dc8092ad3aff75",
+    "url": "./static/media/trombine.85d65506.webp"
+  }
+]);
